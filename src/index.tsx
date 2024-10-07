42217d63837f7ca1a0e48076c429937789b04c1b
Daniil-Ka <dkrapivin.04@gmail.com>
2024-10-07 12:27:13 +0500

    bugfix, главная страница

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import App from './SignIn';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-        <StyledEngineProvider injectFirst>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
             <App />
-        </LocalizationProvider>
-        </StyledEngineProvider>
-        </QueryClientProvider>
     </React.StrictMode>
 );
