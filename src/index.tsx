97a2fa493cc782b14ddb88606e04498ee59da6d2
Daniil-Ka <dkrapivin.04@gmail.com>
2024-10-20 17:56:26 +0500

    фикс

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,8 @@ import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import App from './SignIn';
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.querySelector("#root")!).render(
     <React.StrictMode>
-            <App />
+        <App />
     </React.StrictMode>
 );
